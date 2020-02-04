2b2f8bde8f931e3b3e65dec4f3030748ec242118
Lokesh Verma <44801268+lokesh16191@users.noreply.github.com>
2020-02-04 16:00:51 +0530

    Add files via upload

---
+++ src/main/java/com/lokesh/SpringBoot/Controller/MyController.java
@@ -0,0 +1,24 @@
+package com.lokesh.SpringBoot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping(path="/")
+	public String loadHomePage(){
+		return "Welcome to My First Sprint WebService Page";
+	}
+	
+	@GetMapping(path="/hello")
+	public String hello(){
+		return "Hello World";
+	}
+	@GetMapping(path="/hello/path-variable/{name}")
+	public String HelloWorldPathVariable(@PathVariable String name)
+	{
+		return "Hello World "+name;
+	}
+}
